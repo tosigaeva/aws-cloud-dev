e0324aba82851451fe90e29c0acac804a170fcc1
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2026-06-21 23:48:16 +0200

    feat(task-8): elasticbeanstalk

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://bfr04ikbs1.execute-api.eu-central-1.amazonaws.com/dev",
-  order: "https://pdjha2eq74.execute-api.eu-central-1.amazonaws.com/dev/api/profile/cart",
+  order: "http://tosigaeva-cart-api-prod.eu-central-1.elasticbeanstalk.com/api/profile/cart",
   import: "https://3n1dqwgua4.execute-api.eu-central-1.amazonaws.com/dev/import",
   bff: "https://bfr04ikbs1.execute-api.eu-central-1.amazonaws.com/dev",
-  cart: "https://pdjha2eq74.execute-api.eu-central-1.amazonaws.com/dev/api",
+  cart: "http://tosigaeva-cart-api-prod.eu-central-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
