2270a5cddbbf1bb80e49087439c8c24a3055a6a8
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2026-06-22 00:44:01 +0200

    feat(task-10): 10_backend_for_frontend

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://bfr04ikbs1.execute-api.eu-central-1.amazonaws.com/dev",
-  order: "http://tosigaeva-bff-api-develop.eu-central-1.elasticbeanstalk.com/cart/profile/cart",
+  order: "https://bs3b1ybchb.execute-api.eu-central-1.amazonaws.com/cart/profile/cart",
   import: "https://3n1dqwgua4.execute-api.eu-central-1.amazonaws.com/dev/import",
-  bff: "http://tosigaeva-bff-api-develop.eu-central-1.elasticbeanstalk.com/product",
-  cart: "http://tosigaeva-bff-api-develop.eu-central-1.elasticbeanstalk.com/cart",
+  bff: "https://bs3b1ybchb.execute-api.eu-central-1.amazonaws.com/product",
+  cart: "https://bs3b1ybchb.execute-api.eu-central-1.amazonaws.com/cart",
 };
 
 export default API_PATHS;
